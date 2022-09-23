89d5b0aef96a13a853e79dc229ff6a36cd66ad5e
louisfp <louisjfp@gmail.com>
2022-09-23 15:28:22 +0100

    Put javascript into js file

---
+++ rps-game.js
@@ -0,0 +1,107 @@
+let userScore = 0;
+
+      let computerScore = 0;
+
+      const btns = document.querySelectorAll("button");
+
+      const resetButton = document.querySelector("button.playAgain");
+
+      const resultBox = document.querySelector(".result");
+
+      const userCount = document.querySelector(".userScore");
+
+      const computerCount = document.querySelector(".computerScore");
+
+      const replaceText = document.createTextNode(" ");
+
+      const roundResult = document.querySelector(".roundResult");
+
+      const getComputerChoice = () => {
+        const gameArray = ["Rock", "Paper", "Scissors"];
+        const random_num = Math_random(3);
+        const computerSelection = gameArray[random_num];
+        return computerSelection;
+      };
+
+      //  Making the variables which indicate that the player and computer chose
+      function playRound(playerSelection, computerSelection) {
+        if (playerSelection == "Rock" && computerSelection == "Rock") {
+          roundResult.textContent = "You have tied! You both chose Rock";
+        } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+          computerScore++;
+          roundResult.textContent = "You lose! Paper beats Rock";
+        } else if (
+          playerSelection == "Rock" &&
+          computerSelection == "Scissors"
+        ) {
+          userScore++;
+          roundResult.textContent = "You win! Rock beats Scissors";
+        } else if (
+          playerSelection == "Scissors" &&
+          computerSelection == "Rock"
+        ) {
+          computerScore++;
+          roundResult.textContent = "You lose! Rock beats Scissors";
+        } else if (
+          playerSelection == "Scissors" &&
+          computerSelection == "Paper"
+        ) {
+          userScore++;
+          roundResult.textContent = "You win! Scissors beats Paper";
+        } else if (
+          playerSelection == "Scissors" &&
+          computerSelection == "Scissors"
+        ) {
+          roundResult.textContent = "You have tied! You both chose Scissors";
+        } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+          userScore++;
+          roundResult.textContent = "You win! Paper beats Rock";
+        } else if (
+          playerSelection == "Paper" &&
+          computerSelection == "Scissors"
+        ) {
+          computerScore++;
+          roundResult.textContent = "You lose! Scissors beat Paper";
+        } else if (playerSelection == "Paper" && computerSelection == "Paper") {
+          roundResult.textContent = "You have tied! You both chose Paper";
+        }
+      }
+
+      let computerSelection = getComputerChoice();
+
+      btns.forEach((button) => {
+        button.addEventListener("click", (e) => {
+          let computerSelection = getComputerChoice();
+          playRound(e.target.id, computerSelection);
+          userCount.textContent = "Your Score: " + `${userScore}`;
+          computerCount.textContent =
+            "The enemies' Score: " + `${computerScore}`;
+          if (userScore >= 5) {
+            resultBox.textContent = "Well done! You have beaten the machine!";
+            document.querySelector('#Rock').disabled = true;
+            document.querySelector('#Scissors').disabled = true;
+            document.querySelector('#Paper').disabled = true;
+          }
+          if (computerScore >= 5) {
+            resultBox.textContent =
+            "Oh no! You have been beaten by the machine!";
+            document.querySelector('#Rock').disabled = true;
+            document.querySelector('#Scissors').disabled = true;
+            document.querySelector('#Paper').disabled = true;
+          }
+        });
+      });
+
+      // Helper Functions
+      function normalise(str) {
+        let newString = str[0].toUpperCase() + str.substring(1).toLowerCase();
+        return newString;
+      }
+
+      function Math_random(num) {
+        return Math.floor(Math.random(num) * +num);
+      }
+
+      resetButton.addEventListener("click", () => {
+        document.location.reload();
+      });
